e83cf4421beeb6ecbeb19f1745846dce4a96cf97
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2026-03-07 10:02:02 +0000

    feat: Add contact page route

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+import ContactContent from "@/components/legal/ContactContent/ContactContent";
+
+export const metadata = {
+	title: "Contact",
+};
+
+export default function ContactPage() {
+	return <ContactContent />;
+}
