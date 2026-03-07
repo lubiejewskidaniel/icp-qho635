353b799c8ab64c5318d6c14c4186f493b38c146d
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2026-03-07 11:56:38 +0000

    feat: Add enquiry form page

---
+++ src/app/enquiry/page.js
@@ -0,0 +1,9 @@
+import EnquiryForm from "@/features/enquiry/components/EnquiryForm/EnquiryForm";
+
+export const metadata = {
+	title: "Enquiry",
+};
+
+export default function EnquiryPage() {
+	return <EnquiryForm />;
+}
