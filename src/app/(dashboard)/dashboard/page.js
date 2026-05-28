1319a47d5237488e8635dcbd20e37c9dd9f344e9
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2026-05-28 17:34:52 +0100

    feature(Agent/Manager Dashboard analysis): Change route to be role based

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -1,3 +1,14 @@
+Czyli: import { useAuth } from "@/providers/AuthProvider/AuthProvider";
+
+import ManagerDashboard from "@/features/dashboard/ManagerDashboard";
+import AgentDashboard from "@/features/dashboard/AgentDashboard";
+
 export default function DashboardPage() {
-	return <h1>Overview</h1>;
+	const { role } = useAuth();
+
+	if (role === "manager") {
+		return <ManagerDashboard />;
+	}
+
+	return <AgentDashboard />;
 }
