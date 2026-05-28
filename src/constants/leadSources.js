f514e2eae69f55aeed538802e1c2b77a2472bb2e
ElenaVintila <maya.vintila@yahoo.com>
2026-05-28 14:15:42 +0100

    Add const for sources

---
+++ src/constants/leadSources.js
@@ -0,0 +1,8 @@
+export const LEAD_SOURCES = {
+	WEBSITE: "website",
+	MANUAL: "manual",
+	CSV: "csv",
+	API_WEBHOOK: "api/webhook",
+};
+
+export const LEAD_SOURCE_OPTIONS = Object.values(LEAD_SOURCES);
