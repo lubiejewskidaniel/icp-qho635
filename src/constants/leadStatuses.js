7f511701c0512eb607fa3d207a8fcd4c5509ab46
Giordano Z. <0zanog00@solent.ac.uk>
2026-06-03 22:02:59 +0100

    feat(constants): add leadStatuses constants file.

---
+++ src/constants/leadStatuses.js
@@ -0,0 +1,9 @@
+export const LEAD_STATUSES = {
+	NEW: 'New',
+	CONTACTED: 'Contacted',
+	QUALIFIED: 'Qualified',
+	WON: 'Won',
+	LOST: 'Lost',
+};
+
+export const LEAD_STATUS_OPTIONS = Object.values(LEAD_STATUSES);
